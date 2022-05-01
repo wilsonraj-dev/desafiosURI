03549a80be063a6dcbceeb29207219c76fca77f9
Wilson Junior <wilsonraj@unipam.edu.br>
2022-05-01 20:19:16 -0300

    Criação do projeto

---
+++ desafiosURI/desafiosURI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace desafiosURI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
