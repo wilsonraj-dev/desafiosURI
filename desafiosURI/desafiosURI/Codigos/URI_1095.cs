f4499ed0fc1d8df6b3209f410ca8f38a0692236c
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-09-21 23:39:00 -0300

    Desafios 1095 e 1096

---
+++ desafiosURI/desafiosURI/Codigos/URI_1095.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace desafiosURI.Codigos
+{
+    class URI_1095
+    {
+        /* Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+        Entrada
+        Não há nenhuma entrada neste problema.
+
+        Saída
+        Imprima a sequencia conforme exemplo abaixo */
+
+
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            for (int j = 60; j >= 0; j = j - 5)
+            {
+                Console.WriteLine("I=" + i + " J=" + j);
+                i = i + 3;
+            }
+
+        }
+    }
+}
---
+++ desafiosURI/desafiosURI/Codigos/URI_1096.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace desafiosURI.Codigos
+{
+    class URI_1096
+    {
+        static void Main(string[] args)
+        {
+            /* Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+                
+            Entrada
+            Não há nenhuma entrada neste problema.
+               
+            Saída
+            Imprima a sequencia conforme exemplo abaixo */
+            
+            int i = 1;
+            while (i <= 9)
+            {
+                for (int j = 7; j > 4; j--)
+                {
+                    Console.WriteLine("I=" + i + " J=" + j);
+                }
+                i += 2;
+            }
+        }
+    }
+}
