9be89676063d7367d494138cab063a6cb6310a18
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-09-25 20:13:47 -0300

    Desafio 1144

---
+++ desafiosURI/desafiosURI/Codigos/URI_1144.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafiosURI.Codigos
+{
+    class URI_1144
+    {
+        /* Escreva um programa que leia um valor inteiro N. N * 2 linhas de saída serão apresentadas na execução do programa, seguindo a 
+        lógica do exemplo abaixo. Para valores com mais de 6 dígitos, todos os dígitos devem ser apresentados.
+
+        Entrada
+        O arquivo de entrada contém um número inteiro positivo N (1 < N < 1000).
+
+        Saída
+        Imprima a saída conforme o exemplo fornecido. */
+
+        static void Main(string[] args)
+        {
+            double N = int.Parse(Console.ReadLine());
+            double quadrado = 1;
+            double multiplicador = 0;
+
+            for (double i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"{i} { quadrado = Math.Pow(i, 2)} {multiplicador = i * quadrado}");
+                quadrado++;
+                multiplicador++;
+                Console.WriteLine($"{i} {quadrado} {multiplicador}");
+            }
+        }
+    }
+}
