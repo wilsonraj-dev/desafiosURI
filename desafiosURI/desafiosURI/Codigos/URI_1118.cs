0c0d5dfa7c99533a7ce1e7f6544daa74f01e1a03
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-09-25 20:31:49 -0300

    Desafio 1118

---
+++ desafiosURI/desafiosURI/Codigos/URI_1118.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+
+namespace desafiosURI.Codigos
+{
+    class URI_1118
+    {
+        /* Escreva um programa para ler as notas da primeira e a segunda avaliação de um aluno. Calcule e imprima a média semestral. 
+        O programa só deverá aceitar notas válidas (uma nota válida deve pertencer ao intervalo [0,10]). Cada nota deve ser validada separadamente.
+
+        No final deve ser impressa a mensagem “novo calculo (1-sim 2-nao)”, solicitando ao usuário que informe um código (1 ou 2) 
+        indicando se ele deseja ou não executar o algoritmo novamente, (aceitar apenas os código 1 ou 2). Se for informado o código 1 
+        deve ser repetida a execução de todo o programa para permitir um novo cálculo, caso contrário o programa deve ser encerrado.
+
+        Entrada
+        O arquivo de entrada contém vários valores reais, positivos ou negativos. Quando forem lidas duas notas válidas, deve ser lido um valor inteiro X. 
+        O programa deve parar quando o valor lido para este X for igual a 2.
+
+        Saída
+        Se uma nota inválida for lida, deve ser impressa a mensagem “nota invalida”. Quando duas notas válidas forem lidas, 
+        deve ser impressa a mensagem “media = ” seguido do valor do cálculo.
+
+        Antes da leitura de X deve ser impressa a mensagem "novo calculo (1-sim 2-nao)" e esta mensagem deve ser apresentada novamente
+        se o valor da entrada padrão para X for menor do que 1 ou maior do que 2, conforme o exemplo abaixo.
+
+        A média deve ser impressa com dois dígitos após o ponto decimal. */ 
+
+        static void Main(string[] args)
+        {
+            int opcao = 1;
+
+            while (opcao == 1)
+            {
+                double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                while (n1 < 0 || n1 > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                    n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+
+                double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                while (n2 < 0 || n2 > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                    n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+
+                double media = (n1 + n2) / 2;
+                Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+                Console.WriteLine("novo calculo (1-sim 2-nao)");
+                opcao = int.Parse(Console.ReadLine());
+                while (opcao != 1 && opcao != 2)
+                {
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    opcao = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
