d96c02a66e8450c91770cb0e612bca401b68614e
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-09-28 00:46:26 -0300

    Desafio 1564

---
+++ desafiosURI/desafiosURI/Codigos/1564.cs
@@ -0,0 +1,26 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1564
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1564
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            while ((str = Console.ReadLine()) != null)
+            {
+
+                int x = int.Parse(str);
+                if (x == 0)
+                {
+                    Console.WriteLine("vai ter copa!");
+                }
+                else
+                {
+                    Console.WriteLine("vai ter duas!");
+                }
+            }
+        }
+    }
+}
