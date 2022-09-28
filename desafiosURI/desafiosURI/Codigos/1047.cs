ab3843640ff2b7588a897cadf1e0a992d93cdf59
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-09-28 00:44:39 -0300

    Desafio 2597

--- desafiosURI/desafiosURI/Codigos/1047.cs
+++ desafiosURI/desafiosURI/Codigos/1047.cs
@@ -1,25 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace desafiosURI.Codigos
 {
     class URI_1047
     {
+        /* Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
+               
+        Obs: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.
+               
+        Entrada
+        Quatro números inteiros representando a hora de início e fim do jogo.
+               
+        Saída
+        Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)” . */
+
         static void Main(string[] args)
         {
-            /* Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
-               
-               Obs: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.
-               
-               Entrada
-               Quatro números inteiros representando a hora de início e fim do jogo.
-               
-               Saída
-               Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)” . */
-
             string[] dados = Console.ReadLine().Split(' ');
 
             int horaInicial = int.Parse(dados[0]);
