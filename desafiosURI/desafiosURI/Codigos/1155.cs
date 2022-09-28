f4f7877c859fed25042912865561ed869b56b55e
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-09-28 00:45:47 -0300

    Desafio 1155

---
+++ desafiosURI/desafiosURI/Codigos/1155.cs
@@ -0,0 +1,19 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1155
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1155
+    {
+        static void Main(string[] args)
+        {
+            double S = 1;
+            for (int i = 2; i <= 100; i++)
+            {
+                S = S + 1.00 / i;
+            }
+            var x = Math.Round(S, 2);
+            Console.WriteLine(x.ToString("F2"));
+        }
+    }
+}
