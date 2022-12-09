c8d16581fcc6e95e317134203562f398a4625307
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-08 23:38:03 -0300

    Desafio 1866

---
+++ desafiosURI/desafiosURI/Codigos/1866.cs
@@ -0,0 +1,28 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1866
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1866
+    {
+        static void Main(string[] args)
+        {
+            int c = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < c; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int x = 0;
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (j % 2 == 0)
+                        x++;
+                    else
+                        x--;
+                }
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
