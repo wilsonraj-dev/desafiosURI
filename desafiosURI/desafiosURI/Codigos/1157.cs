7b7ec3f06080fb0768347b0a87b4254f66a1ede5
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-08 23:06:50 -0300

    Desafio 1157

---
+++ desafiosURI/desafiosURI/Codigos/1157.cs
@@ -0,0 +1,21 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1157
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1157
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
