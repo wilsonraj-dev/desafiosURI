c145a1a886ec6408385842fa694be9a192cab30b
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-08 23:03:38 -0300

    Desafio 1146

---
+++ desafiosURI/desafiosURI/Codigos/1146.cs
@@ -0,0 +1,25 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1146
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1146
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i == n)
+                        Console.WriteLine($"{i}");
+                    else
+                        Console.Write($"{i} ");
+                }
+                n = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
