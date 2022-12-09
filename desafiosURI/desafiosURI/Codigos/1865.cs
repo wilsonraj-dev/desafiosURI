0ebe5ef4337ee56b54d3e303e8fcc486553b6d49
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-08 23:25:49 -0300

    Desafio 1865

---
+++ desafiosURI/desafiosURI/Codigos/1865.cs
@@ -0,0 +1,24 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1865
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1865
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] dados = Console.ReadLine().Split(' ');
+                string nome = dados[0];
+
+                if (nome.ToLower().Equals("thor"))
+                    Console.WriteLine("Y");
+                else
+                    Console.WriteLine("N");
+            }
+        }
+    }
+}
