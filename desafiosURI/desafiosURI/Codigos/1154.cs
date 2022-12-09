1ce80213434c3db1bf5c154a1805fae9c542177c
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-08 23:14:54 -0300

    Desafio 1154

---
+++ desafiosURI/desafiosURI/Codigos/1154.cs
@@ -0,0 +1,26 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1154
+using System;
+using System.Globalization;
+namespace desafiosURI.Codigos
+{
+    class _1154
+    {
+        static void Main(string[] args)
+        {
+            double idade = double.Parse(Console.ReadLine());
+            double somaIdades = 0;
+            int quantidadeIdades = 0;
+            double mediaIdades = 0;
+
+            while (idade >= 0)
+            {
+                somaIdades += idade;
+                idade = int.Parse(Console.ReadLine());
+                quantidadeIdades++;
+            }
+
+            mediaIdades = somaIdades / quantidadeIdades;
+            Console.WriteLine(mediaIdades.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
