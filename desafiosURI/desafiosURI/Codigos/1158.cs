033b55ed5754c5f7fbaaf21f82ca5f98c378ac91
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-10 15:10:40 -0300

    Desafio 1156

--- desafiosURI/desafiosURI/Codigos/1158.cs
+++ desafiosURI/desafiosURI/Codigos/1158.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1158
+using System;
 
 namespace desafiosURI.Codigos
 {
