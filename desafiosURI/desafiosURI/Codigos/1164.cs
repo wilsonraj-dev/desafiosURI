4349cff98b9327b9759df716933df27503ec8968
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-10 15:52:07 -0300

    Desafio 1164

---
+++ desafiosURI/desafiosURI/Codigos/1164.cs
@@ -0,0 +1,32 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1164
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1164
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                int soma = 0;
+
+                for (int j = 1; j < x; j++)
+                {
+                    if (x % j == 0)
+                    {
+                        soma += j;
+                    }
+                }
+
+                if (soma == x)
+                    Console.WriteLine($"{x} eh perfeito");
+                else
+                    Console.WriteLine($"{x} nao eh perfeito");
+            }
+        }
+    }
+}
