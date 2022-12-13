d33ee381a4a1a3820ddb972db0f68a776beea681
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-12 22:41:38 -0300

    Desafio 1180

---
+++ desafiosURI/desafiosURI/1180.cs
@@ -0,0 +1,33 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1180
+using System;
+
+namespace desafiosURI
+{
+    class _1180
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] x = new int[n];
+            int menor = 1000;
+            int posicao = 0;
+            string[] aux = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = int.Parse(aux[i]);
+
+                if (menor >= x[i])
+                {
+                    menor = x[i];
+                    posicao = i;
+                }
+            }
+
+            Console.WriteLine($"Menor valor: {menor}");
+            Console.WriteLine($"Posicao: {posicao}");
+        }
+    }
+}
