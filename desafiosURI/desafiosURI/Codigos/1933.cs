6a5b3d012c94553d0cfad290ef8f4e16f99f67e4
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-14 20:57:02 -0300

    Desafio 1933

---
+++ desafiosURI/desafiosURI/Codigos/1933.cs
@@ -0,0 +1,22 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1933
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1933
+    {
+        static void Main(string[] args)
+        {
+            string[] v = Console.ReadLine().Split(' ');
+            int n1 = int.Parse(v[0]);
+            int n2 = int.Parse(v[1]);
+
+            if (n1 == n2)
+                Console.WriteLine(n1);
+            else if (n1 > n2)
+                Console.WriteLine(n1);
+            else if (n2 > n1)
+                Console.WriteLine(n2);
+        }
+    }
+}
