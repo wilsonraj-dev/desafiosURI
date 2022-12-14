3f809c621bec1d4674eba9e5a294f4ad6c3258d2
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-13 23:36:02 -0300

    Desafio 2374

---
+++ desafiosURI/desafiosURI/Codigos/2374.cs
@@ -0,0 +1,17 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2374
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2374
+    {
+        static void Main(string[] args)
+        {
+            int pressaoDesejada = int.Parse(Console.ReadLine());
+            int pressaoDaBomba = int.Parse(Console.ReadLine());
+
+            int resultadoFinal = pressaoDesejada - pressaoDaBomba;
+            Console.WriteLine(resultadoFinal);
+        }
+    }
+}
