33be91695e67b4ec8530f04e8ae0ba0b64d3d6ae
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-14 20:58:00 -0300

    Desafio 2936

---
+++ desafiosURI/desafiosURI/Codigos/2936.cs
@@ -0,0 +1,28 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2936
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2936
+    {
+        static void Main(string[] args)
+        {
+            int curupira = int.Parse(Console.ReadLine());
+            int boitata = int.Parse(Console.ReadLine());
+            int boto = int.Parse(Console.ReadLine());
+            int mapinguari = int.Parse(Console.ReadLine());
+            int iara = int.Parse(Console.ReadLine());
+            int porcaoFinal = 0;
+
+            porcaoFinal = curupira * 300;
+            porcaoFinal += boitata * 1500;
+            porcaoFinal += boto * 600;
+            porcaoFinal += mapinguari * 1000;
+            porcaoFinal += iara * 150;
+            porcaoFinal += 225;
+
+            Console.WriteLine(porcaoFinal);
+
+        }
+    }
+}
