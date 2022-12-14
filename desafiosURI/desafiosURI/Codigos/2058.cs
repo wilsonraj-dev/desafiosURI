0ce82e83400790075a067e68143db3ade8ae0811
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-14 20:58:55 -0300

    Desafio 2058

---
+++ desafiosURI/desafiosURI/Codigos/2058.cs
@@ -0,0 +1,15 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2058
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2058
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int valorMinimo = n - 2;
+            Console.WriteLine(valorMinimo);
+        }
+    }
+}
