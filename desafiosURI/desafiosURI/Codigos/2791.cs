737e800b160b38eab0a479c7e90e184658098ee2
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-15 23:10:58 -0300

    Desafio 2791

---
+++ desafiosURI/desafiosURI/Codigos/2791.cs
@@ -0,0 +1,28 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2791
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2791
+    {
+        static void Main(string[] args)
+        {
+
+            string[] v = Console.ReadLine().Split(' ');
+
+            int c1 = int.Parse(v[0]);
+            int c2 = int.Parse(v[1]);
+            int c3 = int.Parse(v[2]);
+            int c4 = int.Parse(v[3]);
+
+            if (c1 == 1)
+                Console.WriteLine(1);
+            else if (c2 == 1)
+                Console.WriteLine(2);
+            if (c3 == 1)
+                Console.WriteLine(3);
+            if (c4 == 1)
+                Console.WriteLine(4);
+        }
+    }
+}
