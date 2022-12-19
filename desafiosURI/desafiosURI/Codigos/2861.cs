1906ad09d7b8caf20ec98311430dbf0fb9d1e761
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-18 21:56:37 -0300

    Desafio 2861

---
+++ desafiosURI/desafiosURI/Codigos/2861.cs
@@ -0,0 +1,20 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2861
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2861
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string pergunta = Console.ReadLine();
+                Console.WriteLine("gzuz");
+            }
+        }
+    }
+}
