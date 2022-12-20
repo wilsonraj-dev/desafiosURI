293b40a6f9c30823531a5ef93ec31a75d21ac4f9
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-19 22:23:13 -0300

    Desafio 1589

---
+++ desafiosURI/desafiosURI/Codigos/1589.cs
@@ -0,0 +1,23 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1589
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1589
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] v = Console.ReadLine().Split(' ');
+                int r1 = int.Parse(v[0]);
+                int r2 = int.Parse(v[1]);
+                int menorRaio = r1 + r2;
+
+                Console.WriteLine(menorRaio);
+            }
+        }
+    }
+}
