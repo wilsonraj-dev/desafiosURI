5f10985ed38144eda533f4b7c425c5027908cf41
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2022-12-26 23:04:11 -0300

    Desafio 2146

---
+++ desafiosURI/desafiosURI/Codigos/2146.java
@@ -0,0 +1,19 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2146
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()){
+			int senha = sc.nextInt();
+			System.out.println(senha-1);
+		}
+		sc.close();
+ 
+    }
+ 
+}
