23ab2d32bc3f14ede6cd71c247c84da3c423f3d8
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-10 21:55:22 -0300

    Desafio 2455

---
+++ desafiosURI/desafiosURI/Codigos/2455.cs
@@ -0,0 +1,25 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2455
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2455
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int p1 = int.Parse(valores[0]);
+            int c1 = int.Parse(valores[1]);
+            int p2 = int.Parse(valores[2]);
+            int c2 = int.Parse(valores[3]);
+
+            if ((p1 * c1) == (p2 * c2))
+                Console.WriteLine("0");
+            else if ((p1 * c1) < (p2 * c2))
+                Console.WriteLine("1");
+            else
+                Console.WriteLine("-1");
+        }
+    }
+}
