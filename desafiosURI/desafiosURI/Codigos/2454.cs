863ec40775126961ad5ed56543d87daa12c15a8f
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-14 20:10:40 -0300

    Desafio 2454

---
+++ desafiosURI/desafiosURI/Codigos/2454.cs
@@ -0,0 +1,23 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2454
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2454
+    {
+        static void Main(string[] args)
+        {
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int p = int.Parse(valores[0]);
+            int r = int.Parse(valores[1]);
+
+            if (p == 0)
+                Console.WriteLine("C");
+            else if (p == 1 && r == 1)
+                Console.WriteLine("A");
+            else if (p == 1 && r == 0)
+                Console.WriteLine("B");
+        }
+    }
+}
