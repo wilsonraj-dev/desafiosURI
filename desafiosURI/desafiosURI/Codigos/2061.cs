a3e733ec5f692356d52c4d9f3962ef6ed0d087ad
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-15 12:49:50 -0300

    Desafio 2061

---
+++ desafiosURI/desafiosURI/Codigos/2061.cs
@@ -0,0 +1,35 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2061
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2061
+    {
+        static void Main(string[] args)
+        {
+
+            string[] dados = Console.ReadLine().Split(' ');
+            int abas = int.Parse(dados[0]);
+            int acoes = int.Parse(dados[1]);
+
+            for (int i = 0; i < acoes; i++)
+            {
+                string acao = Console.ReadLine();
+
+                if (acao == "fechou")
+                {
+                    abas += 2 - 1;
+                }
+                else if (acao == "clicou")
+                {
+                    abas -= 1;
+                }
+                else
+                {
+                    Console.WriteLine("Valor de entrada inválido");
+                }
+            }
+            Console.WriteLine(abas);
+        }
+    }
+}
