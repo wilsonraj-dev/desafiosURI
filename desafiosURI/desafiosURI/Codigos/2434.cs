2d5ddea8b5c206cf848c084f28d0471c89222758
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-19 23:36:45 -0300

    Desafio 2434

---
+++ desafiosURI/desafiosURI/Codigos/2434.cs
@@ -0,0 +1,28 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2434
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2434
+    {
+        static void Main(string[] args)
+        {
+            string[] dados = Console.ReadLine().Split(' ');
+            int dias = int.Parse(dados[0]);
+            int saldoInicial = int.Parse(dados[1]);
+
+            int menorSaldo = saldoInicial;
+
+            for (int i = 0; i < dias; i++)
+            {
+                int movimentacao = int.Parse(Console.ReadLine());
+                saldoInicial += movimentacao;
+
+                if (saldoInicial < menorSaldo)
+                    menorSaldo = saldoInicial;
+            }
+
+            Console.WriteLine(menorSaldo);
+        }
+    }
+}
