11b682f82335c2f948fda14337072508cae0b913
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-20 20:47:00 -0300

    Desafio 3046

---
+++ desafiosURI/desafiosURI/Codigos/3046.cs
@@ -0,0 +1,17 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/3046
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _3046
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int resultado = ((n + 1) * (n + 2)) / 2;
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
