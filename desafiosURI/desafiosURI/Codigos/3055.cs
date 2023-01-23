9ee654035cd2a46bc72ea8823df341388fb08eb1
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-22 23:16:58 -0300

    Desafio 3055

---
+++ desafiosURI/desafiosURI/Codigos/3055.cs
@@ -0,0 +1,17 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/3055
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _3055
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            int segundaNota = (2 * m) - a;
+            Console.WriteLine(segundaNota);
+        }
+    }
+}
