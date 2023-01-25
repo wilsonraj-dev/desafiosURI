420ca80b2a8ca34897cae927716231a8b22a051d
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-24 22:28:05 -0300

    Desafio 2388

---
+++ desafiosURI/desafiosURI/Codigos/2388.cs
@@ -0,0 +1,23 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2388
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _2388
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int resultado = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] numeros = Console.ReadLine().Split(' ');
+                int t = int.Parse(numeros[0]);
+                int v = int.Parse(numeros[1]);
+                resultado = resultado + (t * v);
+            }
+            Console.WriteLine(resultado);
+        }
+    }
+}
