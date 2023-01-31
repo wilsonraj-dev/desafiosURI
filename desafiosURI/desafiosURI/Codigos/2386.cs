70efb98f6a19ab74204b2cdd135693c55c6e0eb6
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-30 22:58:51 -0300

    Desafio 2386

---
+++ desafiosURI/desafiosURI/Codigos/2386.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafiosURI.Codigos
+{
+    class _2386
+    {
+        static void Main(string[] args)
+        {
+            int telescopeoArea = int.Parse(Console.ReadLine());
+            int numEstrelas = int.Parse(Console.ReadLine());
+            int estrelasVisiveis = 0;
+
+            for (int i = 0; i < numEstrelas; i++)
+            {
+                int fotons = int.Parse(Console.ReadLine());
+
+                if (fotons * telescopeoArea >= 40000000)
+                {
+                    estrelasVisiveis++;
+                }
+            }
+
+            Console.WriteLine(estrelasVisiveis);
+        }
+    }
+}
