1a1128db4418ce2865717c2281894bc503fbf72a
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-03 23:24:20 -0300

    Desafio 3001 - SQL

---
+++ desafiosURI/desafiosURI/DbObjects/3001.sql
@@ -0,0 +1,9 @@
+﻿--https://www.beecrowd.com.br/judge/pt/problems/view/3001
+SELECT name
+     , CASE 
+         WHEN type = 'A' THEN 20.0
+         WHEN type = 'B' THEN 70.0
+         WHEN type = 'C' THEN 530.5
+       END AS price
+        FROM products
+ORDER BY type ASC, id DESC
