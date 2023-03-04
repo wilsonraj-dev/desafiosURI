818e6fe4d2a69965b5ab3ea93e470e6f5664ff8a
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-03-04 20:35:17 -0300

    Desafio 1847

---
+++ desafiosURI/desafiosURI/Codigos/1847.cs
@@ -0,0 +1,26 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1847
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1847
+    {
+        static void Main(string[] args)
+        {
+            string[] temperaturas = Console.ReadLine().Split(' ');
+            int A = int.Parse(temperaturas[0]);
+            int B = int.Parse(temperaturas[1]);
+            int C = int.Parse(temperaturas[2]);
+
+            if (B < A && C >= B) Console.WriteLine(":)");
+            else if (B > A && C <= B) Console.WriteLine(":(");
+            else if (B > A && C > B && (C - B) < (B - A)) Console.WriteLine(":(");
+            else if (B > A && C > B && (C - B) >= (B - A)) Console.WriteLine(":)");
+            else if (B < A && C < B && (B - C) < (A - B)) Console.WriteLine(":)");
+            else if (B < A && C < B && (B - C) >= (A - B)) Console.WriteLine(":(");
+            else if (B == A && C > B) Console.WriteLine(":)");
+            else if (B == A && C < B) Console.WriteLine(":(");
+            else Console.WriteLine(":(");
+        }
+    }
+}
