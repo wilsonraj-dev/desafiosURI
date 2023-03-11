5b6fcf7d039348ba020a2863993107dd5351b1a2
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-03-10 21:09:37 -0300

    Desafio 1072

---
+++ desafiosURI/desafiosURI/Codigos/1072.java
@@ -0,0 +1,23 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1072
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner leitor = new Scanner(System.in);
+    	int N = leitor.nextInt();
+    	int in = 0;
+    	int out = 0;
+    	for (int i = 0; i < N; i++) {
+    		int x = leitor.nextInt();
+    		if (x >= 10 && x <= 20) in++;
+    		else out++;
+    	}
+    	System.out.println(in + " in");
+    	System.out.println(out + " out");
+ 
+    }
+ 
+}
