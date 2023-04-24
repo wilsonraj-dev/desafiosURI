6f6bbdb367aa70b767ad3aead7ea3395bd84d681
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-04-23 21:15:02 -0300

    Desafio 2062

---
+++ desafiosURI/desafiosURI/Codigos/2062.cs
@@ -0,0 +1,27 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2062
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _2062
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] palavra = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                if (palavra[i].Length == 3)
+                {
+                    if (palavra[i].StartsWith("OB") || palavra[i].StartsWith("UR") && !palavra[i].EndsWith("I"))
+                    {
+                        palavra[i] = palavra[i].Substring(0, 2) + "I";
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", palavra));
+        }
+    }
+}
