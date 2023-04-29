30bcc401f470aab3f4e2e0c62d12b5661ef795cc
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-04-28 22:01:48 -0300

    Desafio 1957

---
+++ desafiosURI/desafiosURI/Codigos/1957.cs
@@ -0,0 +1,14 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1957
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _1957
+    {
+        static void Main(string[] args)
+        {
+            int V = int.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(V, 16).ToUpper());
+        }
+    }
+}
