f0607344915693d0e56bc4fdb3c0ea269f2f2525
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-05-04 19:43:38 -0300

    Desafio 1864

---
+++ desafiosURI/desafiosURI/Codigos/1864.cs
@@ -0,0 +1,15 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1864
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _1864
+    {
+        static void Main(string[] args)
+        {
+            string mensagem = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine(mensagem.Substring(0, N));
+        }
+    }
+}
