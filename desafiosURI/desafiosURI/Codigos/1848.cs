beced23f1d2872e076842b57e18f07d4254bca29
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-06-13 22:37:53 -0300

    Desafio 1848

---
+++ desafiosURI/desafiosURI/Codigos/1848.cs
@@ -0,0 +1,38 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1848
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _1848
+    {
+        static void Main(string[] args)
+        {
+            int numero, flag;
+            string corvo = "";
+
+            for (int i = 0; i < 3; i++)
+            {
+                numero = 0;
+                flag = 0;
+
+                while (flag == 0)
+                {
+                    corvo = Console.ReadLine();
+                    switch (corvo)
+                    {
+                        case "--*": numero += 1; break;
+                        case "-*-": numero += 2; break;
+                        case "-**": numero += 3; break;
+                        case "*--": numero += 4; break;
+                        case "*-*": numero += 5; break;
+                        case "**-": numero += 6; break;
+                        case "***": numero += 7; break;
+                        default: flag = 1; break;
+                    }
+                }
+
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
