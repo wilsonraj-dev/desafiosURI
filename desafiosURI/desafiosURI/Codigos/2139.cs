3dff4c31180566ddf838207c9241a23d1e13f32d
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-06-16 23:18:04 -0300

    Desafio 2139

---
+++ desafiosURI/desafiosURI/Codigos/2139.cs
@@ -0,0 +1,37 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2139
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _2139
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                string[] data = input.Split(' ');
+                int mes = int.Parse(data[0]);
+                int dia = int.Parse(data[1]);
+
+                DateTime natal = new DateTime(2016, 12, 25);
+                DateTime dataAtual = new DateTime(2016, mes, dia);
+
+                if (dataAtual == natal)
+                    Console.WriteLine("E natal!");
+                else if (dataAtual == natal.AddDays(-1))
+                    Console.WriteLine("E vespera de natal!");
+                else if (dataAtual > natal)
+                    Console.WriteLine("Ja passou!");
+                else
+                {
+                    TimeSpan diferenca = natal - dataAtual;
+                    int diasFaltando = diferenca.Days;
+                    Console.WriteLine("Faltam {0} dias para o natal!", diasFaltando);
+                }
+            }
+
+        }
+    }
+}
