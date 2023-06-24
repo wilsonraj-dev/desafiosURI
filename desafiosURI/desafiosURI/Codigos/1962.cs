3eaa8228a9907c0221ae7a382d5f3674147f1dbb
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-06-23 23:14:48 -0300

    Desafio 1962

---
+++ desafiosURI/desafiosURI/Codigos/1962.cs
@@ -0,0 +1,27 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1962
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _1962
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int T = int.Parse(Console.ReadLine());
+
+                if (T >= 2015)
+                {
+                    Console.WriteLine((T - 2015) + 1 + " A.C.");
+                }
+                else
+                {
+                    Console.WriteLine(2015 - T + " D.C.");
+                }
+            }
+        }
+    }
+}
