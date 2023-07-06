c9eba157ba2c1da3d77bce22717ab1c86d95e953
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-07-05 22:33:40 -0300

    Desafio 2167

---
+++ desafiosURI/desafiosURI/Codigos/2167.cs
@@ -0,0 +1,37 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2167
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _2167
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] medidas = new int[n];
+            string[] valores = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                medidas[i] = int.Parse(valores[i]);
+            }
+
+            int indiceQueda = EncontrarIndicePrimeiraQueda(medidas);
+
+            Console.WriteLine(indiceQueda);
+        }
+
+        static int EncontrarIndicePrimeiraQueda(int[] medidas)
+        {
+            for (int i = 1; i < medidas.Length; i++)
+            {
+                if (medidas[i] < medidas[i - 1])
+                {
+                    return i + 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
