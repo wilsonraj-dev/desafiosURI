f9c7dcaa3bf2ce74da8f823079415cc43086c58d
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-07 22:07:14 -0300

    Desafio 2003

---
+++ desafiosURI/desafiosURI/Codigos/2003.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace desafiosURI.Codigos
+{
+    public class _2003
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                string[] parts = input.Split(':');
+                int hours = int.Parse(parts[0]);
+                int minutes = int.Parse(parts[1]);
+
+                switch (hours)
+                {
+                    case (7): Console.WriteLine("Atraso maximo: " + minutes); break;
+                    case (8): Console.WriteLine("Atraso maximo: " + (60 + minutes)); break;
+                    case (9): Console.WriteLine("Atraso maximo: " + (120 + minutes)); break;
+                    default: Console.WriteLine("Atraso maximo: 0"); break;
+                }
+            }
+        }
+    }
+}
