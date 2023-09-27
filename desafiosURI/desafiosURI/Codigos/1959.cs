43d2f93dad9fa076bf6e27544d274e7bb0b44c60
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-09-26 22:12:34 -0300

    Desafio 1959

---
+++ desafiosURI/desafiosURI/Codigos/1959.cs
@@ -0,0 +1,19 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1959
+using System;
+
+namespace desafiosURI.Codigos
+{
+    class _1959
+    {
+        static void Main(string[] args)
+        {
+
+            string[] v = Console.ReadLine().Split(' ');
+            long N = long.Parse(v[0]);
+            long L = long.Parse(v[1]);
+
+            long P = N * L;
+            Console.WriteLine(P);
+        }
+    }
+}
