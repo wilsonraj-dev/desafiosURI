4215b5ff2e2dac74f8346242e5f3f36beb22d432
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-10-30 08:38:35 -0300

    Desafio 2483

---
+++ desafiosURI/desafiosURI/Codigos/2483.cs
@@ -0,0 +1,29 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2483
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _2483
+    {
+        static void Main()
+        {
+            int I = int.Parse(Console.ReadLine());
+            string felizNatal = "Feliz natal!!";
+            string felizNatalConvertido = "";
+
+            for (int i = 0; i < felizNatal.Length; i++)
+            {
+                if (i == 9)
+                {
+                    felizNatalConvertido += new string('a', I);
+                }
+                else if (i != 12)
+                {
+                    felizNatalConvertido += felizNatal[i];
+                }
+            }
+
+            Console.WriteLine(felizNatalConvertido);
+        }
+    }
+}
