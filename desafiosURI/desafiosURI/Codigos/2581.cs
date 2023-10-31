07752190ccdce3fbf54092154b00d6d2f9499c25
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-10-31 14:44:46 -0300

    Desafio 2581

---
+++ desafiosURI/desafiosURI/Codigos/2581.cs
@@ -0,0 +1,20 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2581
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _2581
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string pergunta = Console.ReadLine();
+                Console.WriteLine("I am Toorg!");
+            }
+        }
+    }
+}
