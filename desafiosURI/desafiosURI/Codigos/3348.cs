d278b3cec763ad597ed967a0c9e40e6dcf487f8e
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-11-01 13:47:29 -0300

    Desafio 3348

---
+++ desafiosURI/desafiosURI/Codigos/3348.cs
@@ -0,0 +1,26 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/3348
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _3348
+    {
+        static void Main(string[] args)
+        {
+
+            string[] l = new string[]
+            {
+            "2", "7", "5", "30", "169", "441", "1872", "7632", "1740", "93313", "459901", "1358657", "2504881", "13482720", "25779600", "68468401", "610346880", "1271932200", "327280800"
+            };
+
+            int input = int.Parse(Console.ReadLine());
+            int index = input - 1;
+
+            if (index >= 0 && index < l.Length)
+            {
+                Console.WriteLine(l[index]);
+            }
+
+        }
+    }
+}
