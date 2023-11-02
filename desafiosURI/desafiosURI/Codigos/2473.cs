9106578e56fad3bfe616d4242bdb7109d0f0d947
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-11-02 17:25:51 -0300

    Desafio 2473

---
+++ desafiosURI/desafiosURI/Codigos/2473.cs
@@ -0,0 +1,27 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2473
+using System;
+using System.Linq;
+
+namespace desafiosURI.Codigos
+{
+    internal class _2473
+    {
+        public static void Main(string[] args)
+        {
+            string[] numerosFlavinho = Console.ReadLine().Split(' ');
+            string[] numerosSorteados = Console.ReadLine().Split(' ');
+
+            var acertos = numerosFlavinho.Intersect(numerosSorteados);
+            if (acertos.Count() < 3)
+                Console.WriteLine("azar");
+            else if (acertos.Count() == 3)
+                Console.WriteLine("terno");
+            else if (acertos.Count() == 4)
+                Console.WriteLine("quadra");
+            else if (acertos.Count() == 5)
+                Console.WriteLine("quina");
+            else if (acertos.Count() == 6)
+                Console.WriteLine("sena");
+        }
+    }
+}
