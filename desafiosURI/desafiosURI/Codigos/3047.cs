dc638e4156d4ed270c28d633872595178b1f2313
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-11-03 08:44:22 -0300

    Desafio 3047

---
+++ desafiosURI/desafiosURI/Codigos/3047.cs
@@ -0,0 +1,24 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/3047
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _3047
+    {
+        public static void Main(string[] args)
+        {
+            int idadeMonica = int.Parse(Console.ReadLine());
+            int idadeFilho1 = int.Parse(Console.ReadLine());
+            int idadeFilho2 = int.Parse(Console.ReadLine());
+
+            int idadeFilho3 = idadeMonica - (idadeFilho1 + idadeFilho2);
+
+            if (idadeFilho1 > idadeFilho2 && idadeFilho1 > idadeFilho3)
+                Console.WriteLine(idadeFilho1);
+            else if (idadeFilho2 > idadeFilho1 && idadeFilho2 > idadeFilho3)
+                Console.WriteLine(idadeFilho2);
+            else
+                Console.WriteLine(idadeFilho3);
+        }
+    }
+}
