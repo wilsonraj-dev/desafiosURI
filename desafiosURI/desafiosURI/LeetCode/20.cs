f7ab7e1761cbf8def4a47ff876498fb0b208120b
Wilson Junior <wilson.assis.junior@gmail.com>
2024-02-15 20:26:03 -0300

    LeetCode 20

---
+++ desafiosURI/desafiosURI/LeetCode/20.cs
@@ -0,0 +1,38 @@
+﻿//https://leetcode.com/problems/valid-parentheses/description/
+using System.Collections.Generic;
+
+namespace desafiosURI.LeetCode
+{
+    internal class _20
+    {
+        static void Main(string[] args)
+        {
+            IsValid("()[]{}");
+        }
+
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    char top = stack.Pop();
+
+                    if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{'))
+                        return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
