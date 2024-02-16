e639074c8b20519aa71b32c4b713e98bb4afbfc1
Wilson Junior <wilson.assis.junior@gmail.com>
2024-02-16 16:47:13 -0300

    Desafio 3170

---
+++ desafiosURI/desafiosURI/Codigos/3170.cs
@@ -0,0 +1,25 @@
+﻿//https://judge.beecrowd.com/pt/problems/view/3170
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _3170
+    {
+        static void Main(string[] args)
+        {
+            int B = int.Parse(Console.ReadLine());
+            int G = int.Parse(Console.ReadLine());
+
+            if (G % 2 != 0)
+                G -= 1;
+
+            int metade = G / 2;
+            int bolinhas = metade - B;
+
+            if (bolinhas > 0)
+                Console.WriteLine($"Faltam {bolinhas} bolinha(s)");
+            else
+                Console.WriteLine("Amelia tem todas bolinhas!");
+        }
+    }
+}
