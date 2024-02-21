2bfd449fac4049e99afd3445100fb11c93935b93
Wilson Junior <wilson.assis.junior@gmail.com>
2024-02-20 21:57:24 -0300

    Desafio 3174

---
+++ desafiosURI/desafiosURI/Codigos/3174.cs
@@ -0,0 +1,47 @@
+﻿//https://judge.beecrowd.com/pt/problems/view/3174
+using System;
+
+namespace desafiosURI.Codigos
+{
+    internal class _3174
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int countBonecos = 0;
+            int countArquitetos = 0;
+            int countMusicos = 0;
+            int countDesenhistas = 0;
+
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] vt = Console.ReadLine().Split(' ');
+
+                string E = vt[0];
+                string G = vt[1];
+                int H = int.Parse(vt[2]);
+
+                switch (G)
+                {
+                    case "bonecos":
+                        countBonecos += H;
+                        break;
+                    case "arquitetos":
+                        countArquitetos += H;
+                        break;
+                    case "musicos":
+                        countMusicos += H;
+                        break;
+                    case "desenhistas":
+                        countDesenhistas += H;
+                        break;
+                    default: break;
+                }
+            }
+
+            int P = ((countBonecos / 8) + (countArquitetos / 4) + (countMusicos / 6) + (countDesenhistas / 12));
+            Console.WriteLine(P);
+        }
+    }
+}
